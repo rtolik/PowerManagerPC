14d2acc6dd91d6346edb969a97ccaee5e8d112dd
Anatoliy <govier999@gmail.com>
2017-12-11 10:24:23 +0200

    made a feture to turn off the screen before poewer pff

--- PowerManager_v1/Form1.cs
+++ PowerManager_v1/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace PowerManager_v1
 {
@@ -16,6 +11,28 @@ namespace PowerManager_v1
         public Form1()
         {
             InitializeComponent();
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+        }
+
+        public enum MonitorState
+        {
+            MonitorStateOn = -1,
+            MonitorStateOff = 2,
+            MonitorStateStandBy = 1
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+
+        void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            SetMonitorInState(MonitorState.MonitorStateOff);
+        }
+
+        private void SetMonitorInState(MonitorState state)
+        {
+            SendMessage(0xFFFF, 0x112, 0xF170, (int)state);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,12 +48,17 @@ namespace PowerManager_v1
         private void button2_Click(object sender, EventArgs e)
         {
             Process.Start("shutdown", "-a");
+            SetMonitorInState(MonitorState.MonitorStateOn);
             Timer.Stop();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             TimeLeft.Text= (int.Parse(TimeLeft.Text)-1).ToString();
+            if(int.Parse(TimeLeft.Text)==5)
+            {
+                SetMonitorInState(MonitorState.MonitorStateOff);
+            }
         }
     }
 }
